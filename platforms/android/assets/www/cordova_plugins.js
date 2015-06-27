2e45ee17a24ec4534bcdd36c09e20837bdc95a77
huyl <huyl@huyl-macbookpro.roam.corp.google.com>
2015-06-27 16:17:06 -0700

    added preliminary map working

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -13,164 +13,39 @@ module.exports = [
         ]
     },
     {
-        "file": "plugins/cordova-plugin-file-transfer/www/FileTransferError.js",
-        "id": "cordova-plugin-file-transfer.FileTransferError",
+        "file": "plugins/cordova-plugin-console/www/logger.js",
+        "id": "cordova-plugin-console.logger",
         "clobbers": [
-            "window.FileTransferError"
+            "cordova.logger"
         ]
     },
     {
-        "file": "plugins/cordova-plugin-file-transfer/www/FileTransfer.js",
-        "id": "cordova-plugin-file-transfer.FileTransfer",
+        "file": "plugins/cordova-plugin-console/www/console-via-logger.js",
+        "id": "cordova-plugin-console.console",
         "clobbers": [
-            "window.FileTransfer"
+            "console"
         ]
     },
     {
-        "file": "plugins/cordova-plugin-file/www/DirectoryEntry.js",
-        "id": "cordova-plugin-file.DirectoryEntry",
-        "clobbers": [
-            "window.DirectoryEntry"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-file/www/DirectoryReader.js",
-        "id": "cordova-plugin-file.DirectoryReader",
-        "clobbers": [
-            "window.DirectoryReader"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-file/www/Entry.js",
-        "id": "cordova-plugin-file.Entry",
-        "clobbers": [
-            "window.Entry"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-file/www/File.js",
-        "id": "cordova-plugin-file.File",
-        "clobbers": [
-            "window.File"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-file/www/FileEntry.js",
-        "id": "cordova-plugin-file.FileEntry",
-        "clobbers": [
-            "window.FileEntry"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-file/www/FileError.js",
-        "id": "cordova-plugin-file.FileError",
-        "clobbers": [
-            "window.FileError"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-file/www/FileReader.js",
-        "id": "cordova-plugin-file.FileReader",
-        "clobbers": [
-            "window.FileReader"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-file/www/FileSystem.js",
-        "id": "cordova-plugin-file.FileSystem",
-        "clobbers": [
-            "window.FileSystem"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-file/www/FileUploadOptions.js",
-        "id": "cordova-plugin-file.FileUploadOptions",
-        "clobbers": [
-            "window.FileUploadOptions"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-file/www/FileUploadResult.js",
-        "id": "cordova-plugin-file.FileUploadResult",
-        "clobbers": [
-            "window.FileUploadResult"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-file/www/FileWriter.js",
-        "id": "cordova-plugin-file.FileWriter",
-        "clobbers": [
-            "window.FileWriter"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-file/www/Flags.js",
-        "id": "cordova-plugin-file.Flags",
-        "clobbers": [
-            "window.Flags"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-file/www/LocalFileSystem.js",
-        "id": "cordova-plugin-file.LocalFileSystem",
-        "clobbers": [
-            "window.LocalFileSystem"
-        ],
+        "file": "plugins/cordova-plugin-dialogs/www/notification.js",
+        "id": "cordova-plugin-dialogs.notification",
         "merges": [
-            "window"
+            "navigator.notification"
         ]
     },
     {
-        "file": "plugins/cordova-plugin-file/www/Metadata.js",
-        "id": "cordova-plugin-file.Metadata",
-        "clobbers": [
-            "window.Metadata"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-file/www/ProgressEvent.js",
-        "id": "cordova-plugin-file.ProgressEvent",
-        "clobbers": [
-            "window.ProgressEvent"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-file/www/fileSystems.js",
-        "id": "cordova-plugin-file.fileSystems"
-    },
-    {
-        "file": "plugins/cordova-plugin-file/www/requestFileSystem.js",
-        "id": "cordova-plugin-file.requestFileSystem",
-        "clobbers": [
-            "window.requestFileSystem"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-file/www/resolveLocalFileSystemURI.js",
-        "id": "cordova-plugin-file.resolveLocalFileSystemURI",
+        "file": "plugins/cordova-plugin-dialogs/www/android/notification.js",
+        "id": "cordova-plugin-dialogs.notification_android",
         "merges": [
-            "window"
+            "navigator.notification"
         ]
     },
     {
-        "file": "plugins/cordova-plugin-file/www/android/FileSystem.js",
-        "id": "cordova-plugin-file.androidFileSystem",
-        "merges": [
-            "FileSystem"
+        "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+        "id": "cordova-plugin-statusbar.statusbar",
+        "clobbers": [
+            "window.StatusBar"
         ]
-    },
-    {
-        "file": "plugins/cordova-plugin-file/www/fileSystems-roots.js",
-        "id": "cordova-plugin-file.fileSystems-roots",
-        "runs": true
-    },
-    {
-        "file": "plugins/cordova-plugin-file/www/fileSystemPaths.js",
-        "id": "cordova-plugin-file.fileSystemPaths",
-        "merges": [
-            "cordova"
-        ],
-        "runs": true
     }
 ];
 module.exports.metadata = 
@@ -179,8 +54,9 @@ module.exports.metadata =
     "cordova-plugin-whitelist": "1.0.0",
     "cordova-plugin-device": "1.0.1",
     "cordova-plugin-geolocation": "1.0.1",
-    "cordova-plugin-file-transfer": "1.2.0",
-    "cordova-plugin-file": "2.1.0"
+    "cordova-plugin-console": "1.0.1",
+    "cordova-plugin-dialogs": "1.1.1",
+    "cordova-plugin-statusbar": "1.0.1"
 }
 // BOTTOM OF METADATA
 });
--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -1,21 +1,95 @@
+var myLatlng;
+var mapOptions;
+var map;
+var sampleAPI = "https://dl.dropboxusercontent.com/u/72466829/walmart.json";
+var marker;
+var contentString;
+var infowindow;
+var url = 'http://server-api.dquid.io/api/v1/data';
+
 function drawMap() {
-    console.log('draw a basic map');
-    map  = new google.maps.Map(document.getElementById('mapsDiv'), {
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-        center: new google.maps.LatLng(48.33521, 14.32389),
-        zoom: 15
+    myLatlng = new google.maps.LatLng(36.334145, -94.148036);
+    console.log(myLatlng);
+    mapOptions = {
+        zoom: 5,
+        center: myLatlng
+    };
+    map = new google.maps.Map(document.getElementById('mapsDiv'), mapOptions);
+
+    $.getJSON( sampleAPI, {
+        tags: "mount rainier",
+        tagmode: "any",
+        format: "json"
+    })
+    .done(function( data ) {
+        for (var i = 0; i < data.length; i++) {
+            myLatlng = new google.maps.LatLng(data[i].latitude, data[i].longitude);
+
+            marker = new google.maps.Marker({
+                position: myLatlng,
+                map: map,
+                title: data[i].storenum
+            });
+            
+            infowindow = new google.maps.InfoWindow({
+                content: contentString
+            });
+
+            google.maps.event.addListener(marker, 'click', (function(marker, streetaddr, strcity, strstate, zipcode, storenum){
+                return function() {
+                    var sn;
+                    switch (storenum % 3) {
+                        case 0: sn = "115";
+                        break;
+                        case 1: sn = "116";
+                        break;
+                        case 2: sn = "117";
+                        break;
+                    }
+                    var serialNumber = "0000000000000" + sn;
+                    var url3 = url + '/' + serialNumber + '/latest';
+                    function callBack(message) {
+                        contentString = 
+                            '<div id="content">'+
+                                '<div id="siteNotice">'+ '</div>'+
+                                '<h1 id="firstHeading" class="firstHeading">Store' + storenum + '</h1>'+
+                                '<div id="bodyContent">'+
+                                    '<p><b>Street Address:</b></br>' + 
+                                    streetaddr + "</br>" + 
+                                    strcity + ", " + strstate + " " + zipcode +
+                                    '</p>'+
+                                    '<p>' + message + '</p>' +
+                                    '<p>Chart with us: <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+                                    'https:/baytekhackandplay.com</a>'+
+                                '</div>'+
+                            '</div>';
+
+                        infowindow.setContent(contentString);
+                        infowindow.open(map, marker);
+                    }
+
+                    $.ajax({
+                        url: url3,
+                        statusCode: {
+                            200: function(response) {
+                                var prettyPrintResponse = JSON.stringify(response,null,2);
+                                prettyPrintResponse = '200, OK \n\n' + prettyPrintResponse;
+                                callBack(prettyPrintResponse);
+                            },
+                            400: function() {
+                                callBack("400, Bad Request");
+                            },
+                            404: function() {
+                                callBack("404, Not Found");
+                            },
+                        }
+                    });
+                };
+            })(marker, data[i].streetaddr, data[i].strcity, data[i].strstate, data[i].zipcode,
+                 data[i].storenum ));
+        }
     });
-    mymarker = new google.maps.Marker({
-        clickable: false,
-        icon: new google.maps.MarkerImage('http://maps.gstatic.com/mapfiles/mobile/mobileimgs2.png',
-            new google.maps.Size(22,22),
-            new google.maps.Point(0,18),
-            new google.maps.Point(11,11)),
-        shadow: null,
-        zIndex: 999,
-        map: map
-    });
-};
+}
 
 function positionUpdate(pos) {
     console.log('recentering maps');
