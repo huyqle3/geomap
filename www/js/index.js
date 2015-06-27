c8b8e13254b7fc45adb1c524253523f36fd6b9b3
huyl <huyl@huyl-macbookpro.roam.corp.google.com>
2015-06-27 14:49:52 -0700

    add preliminary Google Maps to build from

--- www/js/index.js
+++ www/js/index.js
@@ -1,51 +1,87 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+function drawMap() {
+    console.log('draw a basic map');
+    map  = new google.maps.Map(document.getElementById('mapsDiv'), {
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        center: new google.maps.LatLng(48.33521, 14.32389),
+        zoom: 15
+    });
+    mymarker = new google.maps.Marker({
+        clickable: false,
+        icon: new google.maps.MarkerImage('http://maps.gstatic.com/mapfiles/mobile/mobileimgs2.png',
+            new google.maps.Size(22,22),
+            new google.maps.Point(0,18),
+            new google.maps.Point(11,11)),
+        shadow: null,
+        zIndex: 999,
+        map: map
+    });
+};
+
+function positionUpdate(pos) {
+    console.log('recentering maps');
+    var myLocation = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
+    console.log(myLocation);
+    map.setCenter(myLocation);
+    mymarker.setPosition(myLocation);
+};
+
+function updateLocation(onSuccess) {
+    console.log('updateLocation called');
+    setTimeout(function() {
+        navigator.geolocation.getCurrentPosition(function(pos) {
+            console.log('position successful');
+            positionUpdate(pos);
+            onSuccess();
+        }, function() {
+            console.log('error in updateLocation');
+            navigator.notification.alert('Failed to determine your position are you running emulated?', function() {}, 'Location Error');
+        },
+        { maximumAge: 60000, timeout: 5000, enableHighAccuracy: false });
+    }, 1000);
+};
+
+function startWatch() {
+    console.log('startWatch called');
+    $('#stopWatchLocationBtn').show();
+    $('#watchLocationBtn').hide();
+
+    updateLocation(function() {
+        // setup the watch only iff a first position could get gathered
+        watchId = navigator.geolocation.watchPosition(function(pos) {
+            positionUpdate(pos);
+        }, function(err) {
+            console.log(err);
+            navigator.notification.alert('Failed to watch your location!', function() {}, 'Location Error');
+            stopWatch();
+        });
+    });
+};
+
+function stopWatch() {
+    console.log('stopWatch called');
+    navigator.geolocation.clearWatch(watchId);
+    $('#watchLocationBtn').show();
+    $('#stopWatchLocationBtn').hide();
+};
+
 var app = {
-    // Application Constructor
     initialize: function() {
         this.bindEvents();
     },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
+    
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
+    
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+        $('#stopWatchLocationBtn').hide();
+        if(window.device.platform === 'iOS') {
+            StatusBar.overlaysWebView(false);
+        }
+        console.log('draw map');
+        drawMap();
+        $('#updateLocationBtn').click(updateLocation);
+        $('#watchLocationBtn').click(startWatch);
+        $('#stopWatchLocationBtn').click(stopWatch);
     }
 };
-
-app.initialize();
